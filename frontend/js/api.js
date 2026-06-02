b329b5c7bab95a61cfb22c17d93b6e08931639e0
adtypryg16 <141560118+adtypryg16@users.noreply.github.com>
2026-06-02 13:15:20 +0700

    Update api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const BASE = 'http://localhost:3000/api';
+const BASE = 'http://temanbelajar-production.up.railway.app';
 
 function getToken() { return localStorage.getItem('tb_token'); }
 function setToken(t) { localStorage.setItem('tb_token', t); }
