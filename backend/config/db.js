34c9602e68123ee58b1920d8a1ea9aa43cf8103c
adtypryg16 <141560118+adtypryg16@users.noreply.github.com>
2026-06-02 14:21:21 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host:     process.env.DB_HOST     || 'localhost',
+  host:     process.env.DB_HOST     || 'mysql.railway.internal',
   port:     parseInt(process.env.DB_PORT) || 3306,
   user:     process.env.DB_USER     || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME     || 'temanbelajar',
+  password: process.env.DB_PASSWORD || 'uEmrOvxDGdpTGnZKPVxxyUuydQfAlXhe',
+  database: process.env.DB_NAME     || 'railway',
   waitForConnections: true,
   connectionLimit: 10,
   ssl: process.env.DB_HOST && process.env.DB_HOST !== 'localhost'
